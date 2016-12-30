491d5378e6169abc902d91b05ff8c3764c83c30e
sukan <sukan@sukandeMacBook-Air.local>
2016-12-30 17:50:02 +0800

    邮件中加入图片

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 module.exports = {
     pdfPath: '/report/pdf',
+    imgPath: '/report/img',
     htmlPath: '/report/html',
     basePath: '/data/wpm',
     pages: ['index', 'show', 'movie', 'tv', 'cartoon'],
-    mails: ['sukan@mgtv.com', 'mumu@mgtv.com'],
+    mails: ['sukan@mgtv.com', 'zhigang@mgtv.com', 'xuelian@mgtv.com', 'mumu@mgtv.com'],
     server: 'http://10.100.5.113:8081'
 }
