29de750934ecacc78464d629cf8b6762818d276c
sukan <sukan@sukandeMacBook-Air.local>
2016-12-30 11:31:23 +0800

    调整html路径

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     pdfPath: '/report/pdf',
     htmlPath: '/report/html',
     basePath: '/data/wpm',
-    pages: ['index', 'show'],
+    pages: ['index', 'show', 'movie', 'tv', 'cartoon'],
     mails: ['sukan@mgtv.com', 'mumu@mgtv.com'],
     server: 'http://10.100.5.113:8081'
 }
