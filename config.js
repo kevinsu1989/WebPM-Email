99c858ad0c5e19a7a605b3e65bf81120e43d1f87
sukan <sukan@sukandeMacBook-Air.local>
2016-12-30 10:52:20 +0800

    init

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    pdfPath: '/report/pdf',
+    htmlPath: '/report/html',
+    basePath: '/data/wpm',
+    pages: ['index', 'show'],
+    mails: ['sukan@mgtv.com', 'mumu@mgtv.com'],
+    server: 'http://10.100.5.113:8081'
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+//    Author: 苏衎
+//   E-mail: sukan@e.hunantv.com
+//    Date: 03/25/16 4:55 PM
+//    Description:
+"use strict";
+const _report = require('./lib/report');
+
+_report.init();
